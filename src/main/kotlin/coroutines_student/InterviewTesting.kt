10819f312a65a2b84b761fc11c2089f64d389e78
vyacheslav kuznetsov <vyachekuznetsov@x5.ru>
2024-11-18 15:41:42 +0300

    temp test coroutine file

--- src/main/kotlin/coroutines_student/InterviewTesting.kt
+++ src/main/kotlin/coroutines_student/InterviewTesting.kt
@@ -1,7 +1,23 @@
 package coroutines_student
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
+    val sharedFlow = MutableSharedFlow<Int>()
 
+    launch {
+        repeat(5) {
+            sharedFlow.emit(it)
+            delay(100)
+        }
+    }
+
+    launch {
+        sharedFlow.collect {
+            println(it)
+        }
+    }
 }
